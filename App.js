0173fb369355f691eb863b9a1e12d7d25fcf0038
AndresTIY <aacifuentes@gmail.com>
2020-02-11 09:06:37 -0600

    added config to app file

--- App.js
+++ App.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+const data = [
+  {
+    label: 'TODAY',
+    value: 1,
+  },
+  {
+    label: 'THIS WEEK',
+    value: 120,
+  },
+  {
+    label: 'THIS MONTH',
+    value: 39,
+  },
+  {
+    label: 'YESTERDAY',
+    value: 3,
+  },
+  {
+    label: 'LAST WEEK',
+    value: 25,
+  },
+  {
+    label: 'LAST MONTH',
+    value: 175,
+  },
+];
 
 const App = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Text>Carousel</Text>
+      <Text>Intro Slides Carousel</Text>
+    </View>
+  );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
