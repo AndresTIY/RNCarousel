e541a5fe94f132f04960f65ac7963e7df8845a6a
AndresTIY <aacifuentes@gmail.com>
2020-02-11 09:46:29 -0600

    importing config data

--- App.js
+++ App.js
@@ -1,45 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Carousel from './Carousel';
-
-const data = [
-  {
-    label: 'TODAY',
-    value: 1,
-  },
-  {
-    label: 'THIS WEEK',
-    value: 120,
-  },
-  {
-    label: 'THIS MONTH',
-    value: 39,
-  },
-  {
-    label: 'YESTERDAY',
-    value: 3,
-  },
-  {
-    label: 'LAST WEEK',
-    value: 25,
-  },
-  {
-    label: 'LAST MONTH',
-    value: 175,
-  },
-];
-
-const slideData = [
-  {
-    title: 'Welcome! Swipe to continue',
-  },
-  {
-    title: 'About Feature X',
-  },
-  {
-    title: 'About Feature Y',
-  },
-];
+import {slideData, data} from './config';
 
 const App = () => {
   return (
---
+++ config.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    label: 'TODAY',
+    value: 1,
+  },
+  {
+    label: 'THIS WEEK',
+    value: 120,
+  },
+  {
+    label: 'THIS MONTH',
+    value: 39,
+  },
+  {
+    label: 'YESTERDAY',
+    value: 3,
+  },
+  {
+    label: 'LAST WEEK',
+    value: 25,
+  },
+  {
+    label: 'LAST MONTH',
+    value: 175,
+  },
+];
+
+export const slideData = [
+  {
+    title: 'Welcome! Swipe to continue',
+  },
+  {
+    title: 'About Feature X',
+  },
+  {
+    title: 'About Feature Y',
+  },
+];
